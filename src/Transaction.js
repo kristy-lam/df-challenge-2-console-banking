a28a7a7661b11fcce6e014028beef21dab6d6640
Kristy Lam <lamlokhinkristy@gmail.com>
2024-04-29 17:27:09 +0100

    Updated code to pass test

--- src/Transaction.js
+++ src/Transaction.js
@@ -1,13 +1,15 @@
 export default class Transaction {
     #date;
+    #amount = 0;
 
-    constructor(date) {
+    constructor(date, amount) {
         // Workaround to simulate an abstract class
         if (new.target === Transaction) {
             throw new TypeError('Cannot construct abstract instances directly');
         }
 
         this.#date = date;
+        this.#date = amount;
        
     }
 
@@ -15,6 +17,8 @@ export default class Transaction {
 
     // Setter created for testing only
     setDate = (newDate) => { this.#date = newDate }
+
+    getAmount = () => { return this.#amount }
         
             
 }

9073dc6c9eecf5821dc418f4c532975b711d5591
Kristy Lam <lamlokhinkristy@gmail.com>
2024-05-01 20:05:57 +0100

    Refactored Account Class Tests and started test 5.1

--- spec/Account.spec.js
+++ spec/Account.spec.js
@@ -1,12 +1,20 @@
 import Account from "../src/Account.js";
 
 describe('Account Class Tests: ', () => { 
+    let testAccount;
+
+    beforeEach(() => {
+        testAccount = new Account();
+    });
+
+    afterEach(() => {
+        testAccount = undefined;
+    })
 
     describe('Account Initialisation Tests: ', () => {        
         it('1.1 Should be an instance of the Account class', () => {
             // Arrange
             // Act
-            const testAccount = new Account();
             // Assert
             expect(testAccount).toBeInstanceOf(Account);
         });
@@ -15,7 +23,6 @@ describe('Account Class Tests: ', () => {
             // Arrange            
             const expected = 0;
             // Act
-            const testAccount = new Account();
             // Assert
             expect(testAccount.getBalance()).toEqual(expected);
         });
@@ -24,7 +31,6 @@ describe('Account Class Tests: ', () => {
             // Arrange            
             const expected = {};
             // Act
-            const testAccount = new Account();
             // Assert
             expect(testAccount.getTransactions()).toEqual(expected);
         }); 
@@ -36,15 +42,13 @@ describe('Account Class Tests: ', () => {
             // Arrange            
             const expected = 1000;
             // Act
-            const testAccount = new Account();
             testAccount.deposit('10/01/2012', 1000);
             // Assert
             expect(testAccount.getBalance()).toEqual(expected);
         });
 
         it('1.5 Should only allow a number as amount in the deposit method', () => {
-            // Arrange            
-            const testAccount = new Account();
+            // Arrange
             // Act
             // Assert
             expect(() => (testAccount.deposit('10/01/2012', '1000'))).toThrowError(Error, 'Must provide a number');
@@ -52,15 +56,13 @@ describe('Account Class Tests: ', () => {
 
         it('1.6 Should only allow a positive amount in the deposit method', () => {
             // Arrange            
-            const testAccount = new Account();
             // Act            
             // Assert
             expect(() => (testAccount.deposit('10/01/2012', -500))).toThrowError(Error, 'Must provide a positive amount');
         });
 
         it('1.7 Should only allow the deposit method to run if date is not falsy', () => {
-            // Arrange            
-            const testAccount = new Account();
+            // Arrange 
             // Act            
             // Assert
             expect(() => (testAccount.deposit(undefined, 100))).toThrowError(Error, 'Must provide a date');
@@ -68,7 +70,6 @@ describe('Account Class Tests: ', () => {
 
         it('1.8 Should only allow the deposit method to run if amount is not falsy', () => {
             // Arrange
-            const testAccount = new Account();
             // Act
             // Assert
             expect(() => (testAccount.deposit('10/01/2012', undefined))).toThrowError(Error, 'Must provide an amount');
@@ -76,23 +77,37 @@ describe('Account Class Tests: ', () => {
 
         it('1.9 Should return error message if both date and amount are falsy', () => {
             // Arrange
-            const testAccount = new Account();
             // Act
             // Assert
             expect(() => (testAccount.deposit(undefined, undefined))).toThrowError(Error, 'Must provide inputs');
         });
 
         it('2.6 Should return an array of date, amount, updated balance and type when the deposit method of the Account class is called', () => {
-            // Arrange            
-            const testAccount = new Account();
-            let expected = ['10/01/2012', 1000, 1000, 'deposit'];
+            // Arrange
+            const expected = ['10/01/2012', 1000, 1000, 'deposit'];
             // Act
             // Assert
             expect(testAccount.deposit('10/01/2012', 1000)).toEqual(expected);
         });
 
-        
-
-    });  
-
+        it('2.6 Should return an array of date, amount, updated balance and type when the deposit method of the Account class is called', () => {
+            // Arrange
+            const expected = ['10/01/2012', 1000, 1000, 'deposit'];
+            // Act
+            // Assert
+            expect(testAccount.deposit('10/01/2012', 1000)).toEqual(expected);
+        });    
+    });
+    
+    describe('Withdrawal Method Functionality Tests: ', () => {
+        it('5.1 Should have a withdrawal method with date and amount as arguments which stores the date and adds the amount to the balance', () => {
+            // Arrange
+            testAccount.deposit('10/01/2012', 1000);
+            const expected = 400;
+            // Act
+            testAccount.withdrawal('11/01/2012', 600);
+            // Assert
+            expect(testAccount.getBalance()).toEqual(expected);
+        });
+    });
 });

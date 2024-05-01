5b9b393a28112a849662b14192783c3f538d8aa8
Kristy Lam <lamlokhinkristy@gmail.com>
2024-05-01 11:16:31 +0100

    Added test 1.7

--- spec/Account.spec.js
+++ spec/Account.spec.js
@@ -62,6 +62,16 @@ describe('Account Class Tests: ', () => {
             expect(testAccount.getBalance()).toEqual(expected);
         });
 
+        it('1.7 Should only allow the deposit method to run if date is not empty', () => {
+            // Arrange            
+            const expected = 0;
+            // Act
+            const testAccount = new Account();
+            testAccount.deposit(undefined, 100);
+            // Assert
+            expect(testAccount.getBalance()).toEqual(expected);
+        });
+
         it('2.6 Should return an array of date, amount, updated balance and type when the deposit method of the Account class is called', () => {
             // Arrange            
             const testAccount = new Account();
